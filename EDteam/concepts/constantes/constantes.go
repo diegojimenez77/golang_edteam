854b08ae8db4598498cf74da05326d77de54f2e0
diegojimenez77 <jimpez.diego@gmail.com>
2023-08-03 19:35:06 -0600

    first commit go lang curso basico de EDteam

---
+++ EDteam/concepts/constantes/constantes.go
@@ -0,0 +1,47 @@
+// package main es el paquete principal
+// den donde se ejecuta nuestra aplicacion
+// de EDteam
+
+package main
+
+import "fmt"
+
+func main() {
+	//No es posible usar el operador de variable corta para asignar constantes
+	const os, domain string = "linux", "ed.team"
+
+	//La asignacion de variable dinamica es posible
+	const fruta, numero = "platano", 1
+
+	fmt.Println(os, domain)
+	fmt.Println(fruta, numero)
+
+	const (
+		so      = "Windows"
+		dominio = "de.team"
+	)
+	fmt.Println(so, dominio)
+
+	//iota nos ayuda a asignar valores sequenciales
+	//en el siguiente ejemplo iota nos ayda a darles valor contante a los meses a partir de Feb
+	//las constantes no es necesario usarlas para poder compilar
+	const (
+		Jan = iota + 1
+		Feb
+		Mar
+		Abr
+		May
+		Jun
+	)
+	fmt.Println(Jan, Feb, Mar, Abr, May, Jun)
+
+	//comentario de una sola linea
+	/*comentario multilinea
+	es igual que en javascript
+	los comentarios de bloque se usa para las funciones
+	*/
+
+	// Name es el nombre del profesor de go
+	var Name = "alejandro"
+	fmt.Println(Name)
+}
---
+++ EDteam/concepts/variables/main.go
@@ -0,0 +1,52 @@
+package main
+
+import "fmt"
+
+//Todas las variables declaradas siempre deben ser usadas
+//El codigo siempre debe estar dentro de la funcion main
+
+func main() {
+
+	var apple string = "apple"
+	var banana string = "banana"
+	var naranja string = "orange"
+
+	// apple = ":apple"
+	// banana = ":banana"
+	// naranja = ":orange"
+
+	fmt.Println(apple)
+	fmt.Println(banana)
+	fmt.Println(naranja)
+	fmt.Println(apple, banana, naranja)
+
+	//Agrupacion de variables
+	var (
+		pepino string = "pepino"
+		sandia string = "sandia"
+		mango  string = "mango"
+	)
+	fmt.Println(pepino, sandia, mango)
+
+	//Declaracion y Asignacion en una sola linea
+	var lechuga, brocoli, calabaza string = "lechuga", "brocoli", "calabaza"
+	fmt.Println(lechuga, brocoli, calabaza)
+
+	//Asignacion de variable corto con :=
+	jitomate, pepino, zanahoria := "jitomate", "pepino", "zanahoria"
+	fmt.Println(jitomate, pepino, zanahoria)
+
+	//Asignacion de variable dinamica
+	//Una vez sea asignado el timpo de dato no se va a poder modificar el tipo de dato
+	//El tipo de dato no se puede cambiar pero si el valor
+	//El asignador de variable corto no se puede usar para cambiar el valor uno a uno
+	numero, palomita, sillon := 1, "palomita", "sillon"
+	fmt.Println(numero, palomita, sillon)
+
+	numero = 2
+	fmt.Println(numero, palomita, sillon)
+
+	palomita, limon := "maiz", "limon"
+	fmt.Println(palomita, limon)
+
+}
